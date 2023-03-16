54ccaf83374d7d52d0121dce86d189259355acd9
tokyumilla <tokyumilla@gmail.com>
2023-03-16 18:06:33 +0100

    creación proyecto

---
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/Controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t01.n01.Controllers;
+
+public class HelloWorldController {
+    public String saluda (String nom){
+        return "Hola" + nom + " estàs executant un projecte Maven";
+    }
+
+    public String saluda2 (String nom) {
+        return "Hola" + nom + " estàs executant un projecte Maven";
+    }
+
+
+}
