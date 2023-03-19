d88d896ff12a5d4c1e148216aae860abface9043
tokyumilla <tokyumilla@gmail.com>
2023-03-19 22:19:32 +0100

    método requestparam

--- src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/Controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/Controllers/HelloWorldController.java
@@ -1,7 +1,13 @@
 package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t01.n01.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
-    public String saluda (String nom){
+    @GetMapping("/HelloWorld")
+    public String saluda (@RequestParam(value ="nom", defaultValue = "UNKNOWN",required = false) String nom){
         return "Hola" + nom + " estàs executant un projecte Maven";
     }
 
--- src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/S04T01N01MillaOlayaJuanApplication.java
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/S04T01N01MillaOlayaJuanApplication.java
@@ -3,10 +3,13 @@ package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t01.n01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class S04T01N01MillaOlayaJuanApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(S04T01N01MillaOlayaJuanApplication.class, args);
 	}
 
