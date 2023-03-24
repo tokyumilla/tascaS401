6f65aaa20a2e3115eba9b6b9e38e8d1f31a526af
tokyumilla <tokyumilla@gmail.com>
2023-03-24 21:05:35 +0100

    ejercicio terminado

--- src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/Controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t01/n01/Controllers/HelloWorldController.java
@@ -1,19 +1,25 @@
 package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t01.n01.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/HelloWorld")
     public String saluda (@RequestParam(value ="nom", defaultValue = "UNKNOWN",required = false) String nom){
-        return "Hola" + nom + " estàs executant un projecte Maven";
+        return "Hola " + nom + " estàs executant un projecte Maven";
     }
 
-    public String saluda2 (String nom) {
-        return "Hola" + nom + " estàs executant un projecte Maven";
+@RequestMapping(value = {"/HelloWorld2","/HelloWorld2/{nom}"}, method = RequestMethod.GET)
+    public String saluda2 (@PathVariable(required = false) String nom) {
+        if (nom==null) {
+            return "Hola, estàs executant un projecte Maven";
+        } else {
+            return "Hola " + nom + " estàs executant un projecte Maven";
+        }
+
     }
+
+
 
 
 }
